55f6eaaceca10d442f8267e3a10e0c057e9acd62
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-02-10 20:53:59 +0530

    Assigment level 3 : Block 4

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -15,4 +15,15 @@ var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
 
+while (amount < bank_balance) {
+  amount = amount + PHONE_PRICE;
+
+  if (amount < SPENDING_THRESHOLD) {
+    amount = amount + ACCESSORY_PRICE;
+  }
+}
+
+amount = amount + amount * TAX_RATE;
+console.log(`$${amount.toPrecision(5)}`);
+
 // ⛑ Answer of the above will `$334.76`.
